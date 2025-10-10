9b087ba1b8380183dd528cc789c6d4865ef132fe
Mohit Bagde <natashabagde692@gmail.com>
2025-10-10 18:01:54 +0530

    footer logo link updated.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,9 +10,10 @@ export default function Footer() {
             <div className=' flex flex-wrap gap-y-12  max-w-[1400px] mx-auto  px-10'>
 
                 <div className='flex-1 basis-[300px]'>
-                    <a href='#' className='text-3xl font-bold'>
+
+                    <Link to='/' className='text-3xl font-bold'>
                         Gr<span className='text-orange-500 uppercase'>o</span>cify
-                    </a>
+                    </Link>
 
                     <p className='text-zinc-600 mt-6 max-w-[350px]'>
                         Bred for a high content of beneficial substances. Our products are all fresh and healthy.
@@ -29,7 +30,7 @@ export default function Footer() {
                     </li>
 
                     <li className=' mt-6'>
-                        <Link to="/about"  className='text-zinc-800 hover:text-orange-500 '> About</Link>
+                        <Link to="/about" className='text-zinc-800 hover:text-orange-500 '> About</Link>
                     </li>
 
                     <li className=' mt-6'>
@@ -44,34 +45,34 @@ export default function Footer() {
                     </li>
 
                     <li className=' mt-6'>
-                        <a href="#"  className='text-zinc-800 hover:text-orange-500 '> Support center</a>
+                        <a href="#" className='text-zinc-800 hover:text-orange-500 '> Support center</a>
                     </li>
 
                     <li className=' mt-6'>
                         <a href="#" className='text-zinc-800 hover:text-orange-500 '> Feedback</a>
                     </li>
 
-                     <li className=' mt-6'>
+                    <li className=' mt-6'>
                         <Link to="/contact" className='text-zinc-800 hover:text-orange-500 '> Contact us</Link>
                     </li>
 
                 </ul>
 
                 <div className='flex-1'>
-                      <h5 className='text-zinc-800 text-2xl font-bold'>Stay Connected</h5>
+                    <h5 className='text-zinc-800 text-2xl font-bold'>Stay Connected</h5>
 
-                      <p className='text-zinc-600 mt-6'>
-                        Questions or Feedbacks ? <br/>
+                    <p className='text-zinc-600 mt-6'>
+                        Questions or Feedbacks ? <br />
                         we'd love to hear form you.
-                      </p>
+                    </p>
 
-                      <div className=' flex bg-white p-1 rounded-lg mt-6'>
-                        <input type="email" name="email" id="email" autoComplete='off' placeholder='Email Address' className='h-[5vh] pl-4 flex-1 focus:outline-none'/>
-                         <button className='bg-gradient-to-b from-orange-400 to-orange-500 p-2 rounded-lg text-white text-2xl hover:to-orange-600 cursor-pointer'> 
+                    <div className=' flex bg-white p-1 rounded-lg mt-6'>
+                        <input type="email" name="email" id="email" autoComplete='off' placeholder='Email Address' className='h-[5vh] pl-4 flex-1 focus:outline-none' />
+                        <button className='bg-gradient-to-b from-orange-400 to-orange-500 p-2 rounded-lg text-white text-2xl hover:to-orange-600 cursor-pointer'>
                             <IoIosArrowForward />
-                         </button>
-                      </div>
-                      
+                        </button>
+                    </div>
+
                 </div>
 
 
