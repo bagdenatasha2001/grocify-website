55b484b1059e3fac3b86041553524ed3ac78a0b0
Mohit Bagde <natashabagde692@gmail.com>
2025-10-10 15:44:36 +0530

    Fixed CategoryPage import path issue

--- src/components/Allproducts/Allproducts.jsx
+++ src/components/Allproducts/Allproducts.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import CategoryPage from '../Categorypage/CategoryPage'
+// import CategoryPage from '../../components/CategoryPage/CategoryPage'
 import BgAll from '../../assets/images/all-banner.jpg'
+import CategoryPage from '../Categorypage/CategoryPage'
 
 
 export default function Allproducts() {
   return (
     <div>
-        <CategoryPage  title= "All Products" bgImage={BgAll} categories={['All']} />
+        {/* <CategoryPage  title= "All Products" bgImage={BgAll} categories={['All']} /> */}
+        <CategoryPage   title= "All Products" bgImage={BgAll} categories={['All']}/>
     </div>
   )
 }
